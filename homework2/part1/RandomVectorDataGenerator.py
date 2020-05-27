0b524658681e5ab66c89cd7a482b3f558d24d5e8
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-05-28 04:46:26 +0900

    Expand vector data size from 10^4 to 10^5

--- homework2/part1/RandomVectorDataGenerator.py
+++ homework2/part1/RandomVectorDataGenerator.py
@@ -3,8 +3,8 @@ import random
 
 output_file_vector_data = open("vector_data.txt", "w")
 
-NUM_ELEMENT = 10000
-RANGE = 10000
+NUM_ELEMENT = 100000
+RANGE = 100000
 
 for i in range(0, NUM_ELEMENT):
     output_file_vector_data.write(str(random.randrange(0, RANGE, 1)) + "\n")
