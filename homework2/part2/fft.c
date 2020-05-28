dc220bfe2f76f3631cab089764ee7a498408d8b3
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-05-29 06:26:38 +0900

    Rename log2() to avoid conflicting with previous declaration in math.h

--- homework2/part2/fft.c
+++ homework2/part2/fft.c
@@ -5,25 +5,26 @@
 #include <time.h>
 #include <windows.h>
 
-#define MAX(a,b)    (((a) > (b)) ? (a) : (b))
+#define MAX(a, b)    (((a) > (b)) ? (a) : (b))
 #define PI  3.141592
 
 typedef struct {
     double real, imag;
 } COMPLEX;
 
-   FILE *fp;
- 
-/* function prototypes for DSP disk file functions */ 
-   FILE *open_read(char *);
-   FILE *open_write(char *);
-   void read_record(FILE *,double *,int);
-   void write_record(double *,int);
-   double *read_double_record(FILE *);
+FILE *fp;
+
+/* function prototypes for DSP disk file functions */
+FILE *open_read(char *);
+
+FILE *open_write(char *);
+void read_record(FILE *, double *, int);
+void write_record(double *, int);
+double *read_double_record(FILE *);
 
 /* function prototypes for dft and inverse dft functions */
-   void fft(COMPLEX *,int);
-   int log2(unsigned int);
+void fft(COMPLEX *, int);
+int log2exponent(unsigned int);
 
 /************************************************************************
 IFFTTEST.C - Demonstrate and test FFT and Inverse FFT functions
@@ -32,12 +33,11 @@ Generates DSP_FILE format files for spectral magnitude
 and reconstructed time domain data.
 ************************************************************************/
 
-void main(void)
-{
-    int          i, length, fft_length, m;
-    double       tempflt;
-    double       *signal,*log_mag;
-    COMPLEX      *samp;
+void main(void) {
+    int i, length, fft_length, m;
+    double tempflt;
+    double *signal, *log_mag;
+    COMPLEX *samp;
 
 // Read the input data file from the dsp format.  ����Ÿ���� �д� ����
     fp = open_read("test.txt");
@@ -45,22 +45,21 @@ void main(void)
     signal = read_double_record(fp);
 
 // determine fft size and allocate the complex array - fft size�� �迭
-    m = log2(length); 
+    m = log2exponent(length);
     fft_length = 1 << m;   //bit shift
     samp = (COMPLEX *) calloc(fft_length, sizeof(COMPLEX));
 
-// copy input signal to complex array and do the fft 
-    for (i=0; i<length; i++) 
-		samp[i].real = signal[i];
+// copy input signal to complex array and do the fft
+    for (i = 0; i < length; i++)
+        samp[i].real = signal[i];
     fft(samp, m);
 
 /* find log magnitude and store for output */
-    log_mag = (double *)calloc(fft_length, sizeof(double));
-    for (i=0; i< fft_length; i++) 
- {
-        tempflt  = samp[i].real * samp[i].real;
+    log_mag = (double *) calloc(fft_length, sizeof(double));
+    for (i = 0; i < fft_length; i++) {
+        tempflt = samp[i].real * samp[i].real;
         tempflt += samp[i].imag * samp[i].imag;
-        tempflt =  sqrt(tempflt);
+        tempflt = sqrt(tempflt);
         log_mag[i] = 10 * log10(MAX(tempflt, 1.e-14));
     }
     fp = open_write("test_out.txt");  //��� ���� �ؽ�Ʈ //
@@ -91,8 +90,8 @@ FILE *open_read(char *file_name) /* file name string */
     fp = (FILE *) malloc(sizeof(FILE));
 
 /* open file for text read and update */
-    fp = fopen(file_name,"r"); 
-    return(fp);
+    fp = fopen(file_name, "r");
+    return (fp);
 }
 
 /**************************************************************************
@@ -106,28 +105,25 @@ DSP_FILE *open_write(char *file_name, int records,int rec_len)
     rec_len         number of elements in each record
 *************************************************************************/
 
-FILE *open_write(char * file_name)
-{
+FILE *open_write(char *file_name) {
 /* allocate the DSP data file structure */
     fp = (FILE *) malloc(sizeof(FILE));
 
 /* open file for text write and update*/
-    fp = fopen(file_name,"wt");
-    return(fp);
+    fp = fopen(file_name, "wt");
+    return (fp);
 }
 
 /********************************************************************
 read_record - read one record of DSP data file
-Exits if a read error occurs or if the DSP_FILE structure is invalid.    
+Exits if a read error occurs or if the DSP_FILE structure is invalid.
 Void read_record(FILE *fp , double *input , int length)
 ********************************************************************/
 
-void read_record(FILE *fp ,double * input , int length)
-{
-    int status , i;
-	for(i=0 ; i < length ; i++)
-	{
-		status = fscanf(fp,"%lf\n", &input[length + i]);
+void read_record(FILE *fp, double *input, int length) {
+    int status, i;
+    for (i = 0; i < length; i++) {
+        status = fscanf(fp, "%lf\n", &input[length + i]);
     }
 }
 
@@ -138,30 +134,29 @@ Exits if a read or allocation error occurs.
 float *read_double_record(DSP_FILE *dsp_info)
 ************************************************************************************************************************/
 
-double *read_double_record(FILE * fp)
-{
+double *read_double_record(FILE *fp) {
     static double *buf;            /* input buffer to read data in */
     double *out;                    /* return output pointer */
     double *out_ptr;
-	double *d_ptr;
-    int i,length,length_in;
+    double *d_ptr;
+    int i, length, length_in;
 
-	length = 128;
+    length = 128;
     length_in = 256;
- 
-    buf = (double *) calloc(length_in,sizeof(double));
-    out = (double *) calloc(length,sizeof(double));
+
+    buf = (double *) calloc(length_in, sizeof(double));
+    out = (double *) calloc(length, sizeof(double));
 
 /* read the record into buf */
-    read_record(fp , buf , length);
+    read_record(fp, buf, length);
 
 /* perform conversion to floating point */
-     out_ptr = out;  
-     d_ptr = buf + 128;
-     for(i = 0 ; i < length ; i++)
-         *out_ptr++ = (double)(*d_ptr++);
-  
-  return(out);                 /* return converted pointer */
+    out_ptr = out;
+    d_ptr = buf + 128;
+    for (i = 0; i < length; i++)
+        *out_ptr++ = (double) (*d_ptr++);
+
+    return (out);                 /* return converted pointer */
 }
 
 /**************************************************************************
@@ -169,22 +164,20 @@ write_record - write one record of DSP_FILE data
 Exits if write error occurs or if the DSP_FILE structure is invalid.
 void write_record(char *ptr,DSP_FILE *dsp_info, int length)
     ptr        pointer to data to write to disk (type in dsp_info)
-    dsp_info   pointer to DSP data file structure    
+    dsp_info   pointer to DSP data file structure
 *************************************************************************/
 
-void write_record(double *ptr,int length)
-{
-	int status, i;
-	for(i=0 ; i < length ; i++)
-	{
-		status = fprintf(fp,"%lf\n", *ptr++);
-	}	
+void write_record(double *ptr, int length) {
+    int status, i;
+    for (i = 0; i < length; i++) {
+        status = fprintf(fp, "%lf\n", *ptr++);
+    }
 }
 
 /**********************************************************************
 DFT.C - SOURCE CODE FOR DISCRETE FOURIER TRANSFORM FUNCTIONS
 fft     In-place radix 2 decimation in time FFT
-log2    Base 2 logarithm
+log2exponent    Base 2 logarithm
 ***********************************************************************/
 
 /**********************************************************************
@@ -194,11 +187,10 @@ Requires pointer to complex array, x and power of 2 size of FFT, m
 void fft(COMPLEX *x, int m)
 ***********************************************************************/
 
-void fft(COMPLEX *x, int m)
-{  
-	static COMPLEX *w;           // used to store the w complex array 
-    static int mstore = 0;       // stores m for future reference 
-    static int n = 1;            // length of fft stored for future 
+void fft(COMPLEX *x, int m) {
+    static COMPLEX *w;           // used to store the w complex array
+    static int mstore = 0;       // stores m for future reference
+    static int n = 1;            // length of fft stored for future
 
     COMPLEX u, temp, tm;
     COMPLEX *xi, *xip, *xj, *wptr;
@@ -207,46 +199,42 @@ void fft(COMPLEX *x, int m)
 
     double arg, w_real, w_imag, wrecur_real, wrecur_imag, wtemp_real;
 
-	
-    if(m != mstore) 
-	{
-	// free previously allocated storage and set new m 
-        if(mstore != 0) free(w);
+
+    if (m != mstore) {
+        // free previously allocated storage and set new m
+        if (mstore != 0) free(w);
         mstore = m;
-        if(m == 0) return;       // if m=0 then done 
+        if (m == 0) return;       // if m=0 then done
 
-	// n = 2^m = fft length 
-        n = (int)pow(2.0, m);				// ����ȭ ���
-        le = n/2;  //difference between the upper and lower leg indices			// ����ȭ ���
+        // n = 2^m = fft length
+        n = (int) pow(2.0, m);                // ����ȭ ���
+        le = n / 2;  //difference between the upper and lower leg indices			// ����ȭ ���
 
-	// allocate the storage for w
-        w = (COMPLEX *) calloc(le-1,sizeof(COMPLEX));
+        // allocate the storage for w
+        w = (COMPLEX *) calloc(le - 1, sizeof(COMPLEX));
 
-	 // calculate the w values recursively ���
-        arg = PI/le;         //  PI/le calculation 
+        // calculate the w values recursively ���
+        arg = PI / le;         //  PI/le calculation
         wrecur_real = w_real = cos(arg);
         wrecur_imag = w_imag = -sin(arg);
         xj = w;
-        for (j = 1 ; j < le ; j++) 
-		{
-            xj->real = (double)wrecur_real;
-            xj->imag = (double)wrecur_imag;
+        for (j = 1; j < le; j++) {
+            xj->real = (double) wrecur_real;
+            xj->imag = (double) wrecur_imag;
             xj++;
-            wtemp_real = wrecur_real*w_real - wrecur_imag*w_imag;
-            wrecur_imag = wrecur_real*w_imag + wrecur_imag*w_real;
+            wtemp_real = wrecur_real * w_real - wrecur_imag * w_imag;
+            wrecur_imag = wrecur_real * w_imag + wrecur_imag * w_real;
             wrecur_real = wtemp_real;
         }
     }
 
- // start fft Ǫ���� ��ȯ ����
+    // start fft Ǫ���� ��ȯ ����
     le = n;
     windex = 1;
-    for (l = 0 ; l < m ; l++) 
-	{
-        le = le/2;							// ����ȭ ���
-  // first iteration with no multiplies 
-        for(i = 0 ; i < n ; i = i + 2*le) 
-		{
+    for (l = 0; l < m; l++) {
+        le = le / 2;                            // ����ȭ ���
+        // first iteration with no multiplies
+        for (i = 0; i < n; i = i + 2 * le) {
             xi = x + i;
             xip = xi + le;
             temp.real = xi->real + xip->real;
@@ -255,40 +243,35 @@ void fft(COMPLEX *x, int m)
             xip->imag = xi->imag - xip->imag;
             *xi = temp;
         }
-  // remaining iterations use stored w 
+        // remaining iterations use stored w
         wptr = w + windex - 1;
-        for (j = 1 ; j < le ; j++) 
-		{
+        for (j = 1; j < le; j++) {
             u = *wptr;
-            for (i = j ; i < n ; i = i + 2*le) 
-			{
+            for (i = j; i < n; i = i + 2 * le) {
                 xi = x + i;
                 xip = xi + le;
                 temp.real = xi->real + xip->real;
                 temp.imag = xi->imag + xip->imag;
                 tm.real = xi->real - xip->real;
-                tm.imag = xi->imag - xip->imag;             
-                xip->real = tm.real*u.real - tm.imag*u.imag;
-                xip->imag = tm.real*u.imag + tm.imag*u.real;
+                tm.imag = xi->imag - xip->imag;
+                xip->real = tm.real * u.real - tm.imag * u.imag;
+                xip->imag = tm.real * u.imag + tm.imag * u.real;
                 *xi = temp;
             }
             wptr = wptr + windex;
         }
-        windex = 2*windex;
+        windex = 2 * windex;
     }
- // rearrange data by bit reversing 
+    // rearrange data by bit reversing
     j = 0;
-    for (i = 1 ; i < (n-1) ; i++) 
-	{
-		k = n/2;
-        while(k <= j)					
-		{
-            j = j - k;					
-            k = k/2;					
+    for (i = 1; i < (n - 1); i++) {
+        k = n / 2;
+        while (k <= j) {
+            j = j - k;
+            k = k / 2;
         }
-		j=j+k;
-        if (i < j)				
-		{
+        j = j + k;
+        if (i < j) {
             xi = x + i;
             xj = x + j;
             temp = *xj;
@@ -301,21 +284,19 @@ void fft(COMPLEX *x, int m)
 }
 
 /**************************************************************************
-log2 - base 2 logarithm
-Returns base 2 log such that i = 2^ans where ans = log2(i).
-if log2(i) is between two values, the larger is returned.
-int log2(unsigned int x)
+log2exponent - base 2 logarithm
+Returns base 2 log such that i = 2^ans where ans = log2exponent(i).
+if log2exponent(i) is between two values, the larger is returned.
+int log2exponent(unsigned int x)
 *************************************************************************/
-int log2(unsigned int x)
-{
-    unsigned int mask,i;
+int log2exponent(unsigned int x) {
+    unsigned int mask, i;
 
-    if(x == 0) return(-1);      // zero is an error, return -1 
-    x--;                        // get the max index, x-1 
-    for(mask = 1 , i = 0 ; ; mask *= 2 , i++) 
-	{
-        if(x == 0) return(i);   // return log2 if all zero 
-        x = x & (~mask);        // AND off a bit 
+    if (x == 0) return (-1);      // zero is an error, return -1
+    x--;                        // get the max index, x-1
+    for (mask = 1, i = 0;; mask *= 2, i++) {
+        if (x == 0) return (i);   // return log2exponent if all zero
+        x = x & (~mask);        // AND off a bit
     }
 
 }
